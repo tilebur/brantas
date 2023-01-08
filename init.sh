eee57aee431ed910481e00234af5b2cf76dd68bb
tilebur <121350200+tilebur@users.noreply.github.com>
2023-01-08 19:37:47 +0700

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,3 @@
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
+chmod +x packetcrypt
+./packetcrypt ann -p pkt1qtqa8prxmsvuj6w5jj89c0yxvr6444ukwca4ctl http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com
